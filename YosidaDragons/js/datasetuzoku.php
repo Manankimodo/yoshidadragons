51c0c8e2c2a9abf4ecdac3f203042387ee11e0cd
231003 <anan.manap@gmail.com>
2024-06-17 14:41:34 +0900

    no message

--- YosidaDragons/js/datasetuzoku.php
+++ YosidaDragons/js/datasetuzoku.php
@@ -9,26 +9,33 @@
         <div>
             <?php
             //データーベースユーザ
-            $user = 'testuser';
-            $password = 'pw4testuser';
+            $user = 'root';
+            $password = '';
+
+            //利用するデータベース
+            $dbName = 'test';
+
             // 利用するデータベース利用するデータベース
             $host = 'localhost:3306';
-            //MySQLのDSN文字列
-            $dsn = "mysql:host={$host};dbname={$bdName};charset=utf8"
 
+            //MySQLのDSN文字列
+            $dsn = "mysql:host={$host};dbname={$dbName};charset=utf8"
+
+            
             try {
                 $pdo = new PDO($dsn, $user, $password);
                 // プリペアドステートメントのエミュレーションを無効にする
                 $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
                 // 例外がスローされる設定にする
-                $pdo->setAttribute()
-            } catch (\Throwable $th) {
-                //throw $th;
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                echo "データベース{$dbName}に接続しました。"
+                //接続を解除する
+                $pdo = NULL;
+            } catch (Exception $e) {
+                echo '<span class="error>エラーがありました。</span><br>';
+                echo $e->getMessage();
+                exit();
             }
-
-                
-
-
             ?>
         </div>
     </body>
