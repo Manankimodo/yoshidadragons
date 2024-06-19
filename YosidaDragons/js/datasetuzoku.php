4bc588f08b3a4459676d255e7223e9eb132766d1
231003 <anan.manap@gmail.com>
2024-06-19 14:30:38 +0900

    コード変更

--- YosidaDragons/js/datasetuzoku.php
+++ YosidaDragons/js/datasetuzoku.php
@@ -19,7 +19,7 @@
             $host = 'localhost:3306';
 
             //MySQLのDSN文字列
-            $dsn = "mysql:host={$host};dbname={$dbName};charset=utf8"
+            $dsn = "mysql:host={$host};dbname={$dbName};charset=utf8";
 
             
             try {
@@ -28,11 +28,11 @@
                 $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
                 // 例外がスローされる設定にする
                 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                echo "データベース{$dbName}に接続しました。"
+                echo "データベース{$dbName}に接続しました。";
                 //接続を解除する
                 $pdo = NULL;
             } catch (Exception $e) {
-                echo '<span class="error>エラーがありました。</span><br>';
+                echo "<span class='error'>エラーがありました。</span><br>";
                 echo $e->getMessage();
                 exit();
             }
