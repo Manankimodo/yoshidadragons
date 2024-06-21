863a04e61bea77c44cbc1d025fa9c290a1cfa940
231003 <anan.manap@gmail.com>
2024-06-21 09:11:55 +0900

    昨日はすいませんでした

--- YosidaDragons/html/S06.php
+++ YosidaDragons/html/S06.php
@@ -41,6 +41,36 @@
 
         <!-- テーブル -->
         <table class="speace">
+        <?php
+            //データーベースユーザ
+            $user = 'root';
+            $password = '';
+
+            //利用するデータベース
+            $dbName = 'test';
+
+            // 利用するデータベース利用するデータベース
+            $host = 'localhost:3306';
+
+            //MySQLのDSN文字列
+            $dsn = "mysql:host={$host};dbname={$dbName};charset=utf8";
+
+            
+            try {
+                $pdo = new PDO($dsn, $user, $password);
+                // プリペアドステートメントのエミュレーションを無効にする
+                $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+                // 例外がスローされる設定にする
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                echo "データベース{$dbName}に接続しました。";
+                //接続を解除する
+                $pdo = NULL;
+            } catch (Exception $e) {
+                echo "<span class='error'>エラーがありました。</span><br>";
+                echo $e->getMessage();
+                exit();
+            }
+        ?>
             <tr>
                 <th>NO</th>
                 <th>ID</th>
