6e8d2d5c3fc1c2fa694a643063df9afe49aceea5
231003 <anan.manap@gmail.com>
2024-07-01 15:15:58 +0900

    敗北者

--- YosidaDragons/html/S07.php
+++ YosidaDragons/html/S07.php
@@ -25,7 +25,7 @@
             </div> 
             <div class="speace">
                 <form action="">
-                    <input type="text" name="" id="">
+                    <input type="text" name="isbn" id="">
                     <p>例:44444444</p>
                 </form>
             </div>
@@ -38,20 +38,20 @@
             </div> 
             <div class="speace">
                 <form action="">
-                    <input type="text" name="" id="">
+                    <input type="text" name="title" id="">
                     <p>例:人が眠る</p>
                 </form>
             </div>
         </div>
 
-        <!-- キーワード -->
+        <!-- 会社名 -->
         <div class="flex">
             <div class="text speace">
-                <p>キーワード</p>
+                <p>会社名</p>
             </div> 
             <div class="speace">
                 <form action="">
-                    <input type="text" value="東京都新宿区××××××××××××" name="" id="">
+                    <input type="text" value="不倫会社" name="publisher" id="">
                 </form>
             </div>
         </div>
@@ -68,44 +68,72 @@
         </div>
 
         <!-- テーブル -->
-        <div>
-            <table>
-                <tr>
-                    <th>ID</th>
-                    <th>ISBN</th>
-                    <th>タイトル</th>
-                    <th>著者名</th>
-                    <th>出版社</th>
-                </tr>
-                <tr>
-                    <td>test</td>
-                    <td>test</td>
-                    <td>test</td>
-                    <td>test</td>
-                    <td>test</td>
-                </tr>
-                <tr>
-                    <td>　</td>
-                    <td>　</td>
-                    <td>　</td>
-                    <td>　</td>
-                    <td>　</td>
-                </tr>
-                <tr>
-                    <td>　</td>
-                    <td>　</td>
-                    <td>　</td>
-                    <td>　</td>
-                    <td>　</td>
-                </tr>
-            </table>
-        </div>
+        <div class="speace">
+        <table class="Table">
+        <?php 
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                // form method = postからデータを受け取る
+                $Tel = $_POST['tel'];
+                $name = $_POST['name'];
+                $publisher = $_POST['publisher'];
+            
+                try {
+                    // データベースに接続
+                    $pdo = new PDO($dsn, $user, $password);
+                    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+                    // SQLクエリの準備と実行
+                    $sql = "SELECT DISTINCT B.* FROM books A, customers B, cust_subscribe C WHERE kana=:name AND tel=:Tel";
+                    $stm = $pdo->prepare($sql);
+                    $stm->bindParam(':Tel', $Tel, PDO::PARAM_STR);
+                    $stm->bindParam(':name', $name, PDO::PARAM_STR);
+                    $stm->bindParam(':publisher', $publisher, PDO::PARAM_STR);
+                    $stm->execute();
+            
+                    $result = $stm->fetchAll(PDO::FETCH_ASSOC);
+                } catch (Exception $e) {
+                    echo '<span>エラー</span><br>';
+                    echo $e->getMessage();
+                    exit();
+                }
+            }
+                
 
+                
+                // $sql = "SELECT DISTINCT B.*FROM books A, customers B, cust_subscribe C WHERE name=:name AND tel = :Tel";
+                // $stm->bindParam(':Tel', $Tel, PDO::PARAM_INT); // :Tel としてプレースホルダーを使用する
+                // $stm->bindParam(':name', $name, PDO::PARAM_INT); // :name としてプレースホルダーを使用する
+                // $stm = $pdo->prepare($sql);
+                // $stm->execute();
+                
+                // $result = $stm->fetchAll(PDO::FETCH_ASSOC);
+                
+                echo "<thead><tr>";
+                echo "<th>ID</th><th>書籍番号</th><th>タイトル</th><th>著者</th><th>会社名</th><th>在庫</th><th>価格</th>";
+                echo "</tr></thead>";
+                
+                echo "<tbody>";
+                #es関数の処理は上のfunction esに記述している
+
+                foreach ($result as $row) {
+                    echo "<tr>";
+                    echo "<td>", es($row['book_id']), "</td>";
+                    echo "<td>", es($row['isbn']), "</td>";
+                    echo "<td>", es($row['tytle']), "</td>";
+                    echo "<td>", es($row['author_name']), "</td>";
+                    echo "<td>", es($row['publisher']), "</td>";
+                    echo "<td>", es($row['stock']), "</td>";
+                    echo "<td>", es($row['price']), "</td>";
+                    echo "</tr>";     
+                }
+                echo "</tbody>"; 
+            ?>
+        </table>
+    </div>
         <!-- 戻るボタン -->
         <div id="back_button">
             <a class="Button" href="S01.php">戻る</a>
         </div>
-        
     </div>
 </body>
 </html>
