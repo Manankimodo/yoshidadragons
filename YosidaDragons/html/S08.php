97049cf553f94744ee192f947f39ff5c5ccfb7e9
231003 <anan.manap@gmail.com>
2024-07-03 11:59:39 +0900

    no message

--- YosidaDragons/html/S08.php
+++ YosidaDragons/html/S08.php
@@ -1,3 +1,20 @@
+<?php
+$user = 'root';
+$password = '';
+
+// 利用するデータベース
+$dbName = 'test';
+$host = 'localhost:3306';
+
+// MySQLのDSN文字列
+$dsn = "mysql:host={$host};dbname={$dbName};charset=utf8";
+
+// HTMLエスケープ用の関数
+function es($data) {
+    return htmlspecialchars($data, ENT_QUOTES, 'UTF-8');
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -41,35 +58,35 @@
                 <!-- 著者名 -->
                 <div class="flex speace">
                     <p>著者名</p>
-                    <input value="鈴木涼子" type="text">
+                    <input value="鈴木涼子" type="text" name="" id="">
                 </div>
 
                 <!-- 出版社 -->
                 <div class="flex speace">
                     <p>出版社</p>
-                    <input value="門山社" type="text">
+                    <input value="門山社" type="text" name="" id="">
                 </div>
 
                 <!-- 出版日 -->
-                <div class="flex speace">
+                <div class="flex speace" name="" id="">
                     <p>出版日</p>
                     <input value="20xx/01/21" type="text">
                 </div>
 
                 <!-- 価格(円) -->
-                <div class="flex speace">
+                <div class="flex speace" name="" id="">
                     <p>価格(円)</p>
                     <input value="980" type="text">
                 </div>
 
                 <!-- ジャンル -->
-                <div class="flex speace">
+                <div class="flex speace" name="" id="">
                     <p>ジャンル</p>
                     <input value="ミステリー" type="text">
                 </div>
 
                 <!-- 定期購読社数 -->
-                <div class="flex speace">
+                <div class="flex speace" name="" id="">
                     <p>定期購読社数</p>
                     <p>6人</p>
                 </div>
